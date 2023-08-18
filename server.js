21f62013ace7a27ea170ffd70581cd98965fb9a2
DARSHAN-THE-CODER <reachdarshanv@gmail.com>
2023-08-18 16:39:02 +0530

    test

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ const baseRouter = express.Router();
 
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello world!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    res.json({ result: req.body.first + req.body.second });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    res.json({ result: req.body.first - req.body.second });
 });
 
 app.use(baseUrl, baseRouter);
